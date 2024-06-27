c95aa5f581c2acd80546cf273b2dab69d4d1f9fe
Leonardo Santos <leodossantoslds@gmail.com>
2024-06-27 14:53:24 -0300

    checkout flow

--- src/routes/RestaurantRoute.ts
+++ src/routes/RestaurantRoute.ts
@@ -5,6 +5,16 @@ import RestaurantController from "../controllers/RestaurantController";
 const router = express.Router();
 
 router.get(
+  "/:restaurantId",
+  param("restaurantId")
+    .isString()
+    .trim()
+    .notEmpty()
+    .withMessage("restaurantId parameter must ve a valid string"),
+  RestaurantController.getRestaurant
+);
+
+router.get(
   "/search/:city",
   param("city")
     .isString()
